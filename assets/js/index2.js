a8726bacc34db39139d349cfb1bffaedf4608d37
ochanman <david.godignon@lapiscine.pro>
2021-12-06 00:51:45 +0100

    header responsive

--- assets/js/index2.js
+++ assets/js/index2.js
@@ -1,7 +1,10 @@
 
 const bannerR = document.querySelector (".bannerR");
 const bannerL = document.querySelector (".bannerL");
-
+const btnTL = document.querySelector (".btnTL");
+const btnBL = document.querySelector (".btnBL");
+const btnTR = document.querySelector (".btnTR");
+const btnBR = document.querySelector (".btnBR");
 
 
 window.addEventListener("scroll", (e) => {
@@ -17,6 +20,20 @@ window.addEventListener("scroll", (e) => {
 
 })
 
+window.addEventListener("resize", (e) => {
+
+  let btnWitdh = window.innerWidth/4;
+  btnTL.style.width = btnWitdh + "px";
+  btnTL.style.height = btnWitdh/2 + "px";
+  btnBL.style.width = btnWitdh + "px";
+  btnBL.style.height = btnWitdh/2 + "px";
+  btnTR.style.width = btnWitdh + "px";
+  btnTR.style.height = btnWitdh/2 + "px";
+  btnBR.style.width = btnWitdh + "px";
+  btnBR.style.height = btnWitdh/2 + "px";
+  btn.style.width = btnWitdh + "px";
+  btn.style.height = btnWitdh/2 + "px";
+})
 
 
 
